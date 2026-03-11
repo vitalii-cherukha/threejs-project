101d480a74ace924199df32be99c5562a2af9cf6
vitalii_cherukha <vitalii.cherukha@gmail.com>
2026-03-11 12:00:44 +0200

    feat: enhance material properties and add new textures for improved rendering

--- src/main-2.ts
+++ src/main-2.ts
@@ -38,6 +38,9 @@ const doorHeightTexture = textureLoader.load(
 const doorNormalTexture = textureLoader.load(
   "../static/textures/door/normal.jpg",
 );
+const doorMetalnessTexture = textureLoader.load(
+  "../static/textures/door/metalness.jpg",
+);
 const doorRoughnessTexture = textureLoader.load(
   "../static/textures/door/roughness.jpg",
 );
@@ -90,8 +93,17 @@ const gradientTexture = textureLoader.load(
 
 //MeshStandardMaterial
 const material = new THREE.MeshStandardMaterial();
-material.metalness = 0.7;
-material.roughness = 0.2;
+material.metalness = 1;
+material.roughness = 1;
+material.map = doorColorTexture;
+material.aoMap = doorAmbientOcclusionTexture;
+material.aoMapIntensity = 1;
+material.displacementMap = doorHeightTexture;
+material.displacementScale = 0.1;
+material.metalnessMap = doorMetalnessTexture;
+material.roughnessMap = doorRoughnessTexture;
+material.normalMap = doorNormalTexture;
+material.normalScale.set(0.5, 0.5);
 
 gui.add(material, "metalness").min(0).max(1).step(0.001);
 gui.add(material, "roughness").min(0).max(1).step(0.001);
@@ -99,25 +111,26 @@ gui.add(material, "roughness").min(0).max(1).step(0.001);
 const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 16, 16), material);
 sphere.position.x = -1.5;
 
-const plane = new THREE.Mesh(new THREE.PlaneGeometry(1, 1), material);
-plane.position.x = 1.5;
+const plane = new THREE.Mesh(new THREE.PlaneGeometry(1, 1, 100, 100), material);
+plane.position.x = 0;
 
 const torus = new THREE.Mesh(
   new THREE.TorusGeometry(0.3, 0.2, 16, 32),
   material,
 );
+torus.position.x = 1.5;
 
 scene.add(sphere, plane, torus);
 
 //Light
-const ambientLight = new THREE.AmbientLight(0xffffff, 1);
-scene.add(ambientLight);
+// const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+// scene.add(ambientLight);
 
-const pointLight = new THREE.PointLight(0xffffff, 30);
-pointLight.position.x = 2;
-pointLight.position.y = 3;
-pointLight.position.z = 4;
-scene.add(pointLight);
+// const pointLight = new THREE.PointLight(0xffffff, 30);
+// pointLight.position.x = 2;
+// pointLight.position.y = 3;
+// pointLight.position.z = 4;
+// scene.add(pointLight);
 
 //Environment map
 const rgbeLoader = new RGBELoader();
@@ -126,6 +139,7 @@ rgbeLoader.load(
   (environmentMap) => {
     environmentMap.mapping = THREE.EquirectangularReflectionMapping;
     scene.background = environmentMap;
+    scene.environment = environmentMap;
   },
 );
 
