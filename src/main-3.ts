25ce302bacf738745d9de0156d457d82b105b9b1
vitalii_cherukha <vitalii.cherukha@gmail.com>
2026-03-12 18:09:54 +0200

    feat: implement FontLoader and TextGeometry for enhanced text rendering

--- src/main-3.ts
+++ src/main-3.ts
@@ -2,7 +2,7 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import GUI from "lil-gui";
 import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
-import typefaceFont from "three/examples/fonts/helvetiker_regular.typeface.json";
+import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js";
 
 /**
  * Base
@@ -26,15 +26,27 @@ const textureLoader = new THREE.TextureLoader();
  */
 const fontLoader = new FontLoader();
 
+fontLoader.load("../static/fonts/helvetiker_regular.typeface.json", (font) => {
+  const textGeometry = new TextGeometry("Fanya", {
+    font: font,
+    size: 0.5,
+    depth: 0.2,
+    curveSegments: 12,
+    bevelEnabled: true,
+    bevelThickness: 0.03,
+    bevelSize: 0.02,
+    bevelOffset: 0,
+    bevelSegments: 5,
+  });
+  const textMaterial = new THREE.MeshBasicMaterial();
+  textMaterial.wireframe = true;
+  const text = new THREE.Mesh(textGeometry, textMaterial);
+  scene.add(text);
+});
+
 /**
  * Object
  */
-const cube = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial(),
-);
-
-scene.add(cube);
 
 /**
  * Sizes
