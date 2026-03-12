8f100f4cd0709d1563e5b91cb2a5105757197e6f
vitalii_cherukha <vitalii.cherukha@gmail.com>
2026-03-12 11:21:29 +0200

    feat: add typeface font for improved text rendering

--- src/main-3.ts
+++ src/main-3.ts
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import GUI from "lil-gui";
+import typefaceFont from "three/examples/fonts/helvetiker_regular.typeface.json";
 
 /**
  * Base
