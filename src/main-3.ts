da39497d98ffd7147340729609a85a343df4c733
vitalii_cherukha <vitalii.cherukha@gmail.com>
2026-03-16 15:31:11 +0200

    feat: update matcap texture and add randomised donut meshes to the scene

--- src/main-3.ts
+++ src/main-3.ts
@@ -25,7 +25,7 @@ scene.add(axesHelper);
  * Textures
  */
 const textureLoader = new THREE.TextureLoader();
-const matcapTexture = textureLoader.load("../static/textures/matcaps/1.png");
+const matcapTexture = textureLoader.load("../static/textures/matcaps/5.png");
 matcapTexture.colorSpace = THREE.SRGBColorSpace;
 
 /**
@@ -56,11 +56,31 @@ fontLoader.load("../static/fonts/helvetiker_regular.typeface.json", (font) => {
     );
   }
 
-  const textMaterial = new THREE.MeshMatcapMaterial();
-  textMaterial.matcap = matcapTexture;
+  const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture });
+  // textMaterial.matcap = matcapTexture;
   // textMaterial.wireframe = true;
-  const text = new THREE.Mesh(textGeometry, textMaterial);
+  const text = new THREE.Mesh(textGeometry, material);
   scene.add(text);
+
+  const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45);
+
+  for (let i = 0; i < 200; i++) {
+    const donut = new THREE.Mesh(donutGeometry, material);
+    donut.position.x = (Math.random() - 0.5) * 10;
+    donut.position.y = (Math.random() - 0.5) * 10;
+    donut.position.z = (Math.random() - 0.5) * 10;
+
+    donut.rotation.x = Math.random() * Math.PI;
+    donut.rotation.y = Math.random() * Math.PI;
+
+    const scale = Math.random();
+    // donut.scale.x = scale;
+    // donut.scale.y = scale;
+    // donut.scale.z = scale;
+    donut.scale.set(scale, scale, scale);
+
+    scene.add(donut);
+  }
 });
 
 /**
